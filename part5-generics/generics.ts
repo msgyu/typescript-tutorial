23fd5b6ddd6fbf640ae7ebaa3e85fce34c1ebb68
柳生真輝 <nousidayo@gmail.com>
2022-02-23 17:23:01 +0900

    classにGenelicsを利用、またUtility型を活用した例を学習

--- part5-generics/generics.ts
+++ part5-generics/generics.ts
@@ -46,8 +46,75 @@ function copyByKeyof<T extends { name: string}, U extends keyof T>(value: T, key
 
 console.log(copyByKeyof({ name: 'Quill', age: 8}, 'name')); // Tのオブジェクトのkeyを並べたUnion型がUで、nameは第一引数に含まれるため、エラーなし。
 console.log(copyByKeyof({ name: 'Quill', age: 8}, 'age'));  // Tのオブジェクトのkeyを並べたUnion型がUで、ageは第一引数に含まれるため、エラーなし。
-console.log(copyByKeyof({ name: 'Quill', age: 8}, 'foo'));  // fooは第一引数のkeyにないためエラー。
+// console.log(copyByKeyof({ name: 'Quill', age: 8}, 'foo'));  // fooは第一引数のkeyにないためエラー。
 
 
+
+
+/**
+ * Genelicsをclassで利用する
+ */
+
+
+class LightDatabase<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    add(item: T) {
+        this.data.push(item);
+    }
+
+    remove(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+console.log(stringLightDatabase.get());
+
+
+/**
+ * Utility型
+ * 型のライブラリ。importして利用することができる。
+ */
+
+
+interface Todo {
+    title: string;
+    text: string;
+}
+
+type Todoable = Partial<Todo>;
+// type Todoable = {
+//     title?: string | undefined; 
+//     text?: string | undefined;
+// }
+
+/**
+ * readonlyにする
+ */
+type ReadTodo = Readonly<Todo>;
+// type ReadTodo = {
+//     readonly title: string; 
+//     readonly text: string;
+// }
+
+
+const fetchData: Promise<string> = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('hello');
+    }, 3000)
+});
+
+fetchData.then(data => {
+    data
+})
+
+const vegetables: Array<string> = ['Tomato', 'Broccoli'];
 
 
